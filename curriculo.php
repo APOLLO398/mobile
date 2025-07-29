095eb667361d592264c892492b91d53e3dbc26cf
APOLLO398 <raul.figueiredo2015@gmail.com>
2025-07-29 09:26:15 -0300

    Create curriculo.php

---
+++ curriculo.php
@@ -0,0 +1,23 @@
+<?php
+$empresa = @_POST[];
+$empresa = @_POST[];
+$empresa = @_POST[];
+$empresa = @_POST[];
+
+
+
+
+
+
+
+$empresa = @_POST["empresa"];
+$empresa = @_POST["data_admissao"];
+$empresa = @_POST["data_demissao"];
+$empresa = @_POST["atividade_exercida"];
+
+$empresa = @_POST["descricao"];
+$empresa = @_POST["horas"];
+$empresa = @_POST["ano_conclusao"];
+
+$empresa = @_POST["informacoes"];
+?>
